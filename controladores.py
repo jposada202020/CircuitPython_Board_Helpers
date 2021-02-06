1545961e67ed1f0f5ef34190d3cc2f45146ef50e
jposada202020 <34255413+jposada202020@users.noreply.github.com>
2021-02-06 01:10:35 -0500

    Update controladores.py

--- controladores.py
+++ controladores.py
@@ -5,6 +5,15 @@ import board
 def get_pin_definition(pinid):
     controller = uname()[0]
     machine = uname()[4]
+        if controller == 'rp2040':
+    	boardspins = {'A0': board.A0, 'A1': board.A1, 'A2':board.A2, 'GP0': board.GP0, 'GP1': board.GP1, 'GP10': board.GP10,
+    		 'GP11': board.GP11, 'GP12': board.GP12, 'GP13': board.GP13, 'GP14': board.GP14, 'GP15': board.GP15,
+    		 'GP16': board.GP16, 'GP17': board.GP17, 'GP18': board.GP18, 'GP19': board.GP19, 'GP2': board.GP15,
+    		 'GP20': board.GP20, 'GP21': board.GP21, 'GP22': board.GP22, 'GP25': board.GP25, 'GP26': board.GP26,
+    		 'GP26_A0': GP26_A0, 'GP27': board.GP27, 'GP27_A1': board.GP27_A1, 'GP28': board.GP28, 'GP28_A2': board.GP28_A2,
+    		 'GP3': board.GP3, 'GP4': board.GP4, 'GP5':board.GP5, 'GP6': board.GP6, 'GP7': board.GP7, 'GP8': board.GP8,
+    		 'GP9': board.GP9, 'LED': board.LED}
+        return boardspins[pinid]
     if controller == 'esp32s2':
         boardspins = {'A0': board.A0, 'A1': board.A1, 'A2': board.A2, 'A3': board.A3, 'A4': board.A4, 'A5': board.A5,
                 'IO1': board.IO1, 'IO2': board.IO2, 'IO3': board.IO3, 'IO4': board.IO4, 'IO5': board.IO5, 'IO6': board.IO6,
